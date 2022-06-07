ae769aeec55953b70e2c0c00e9f48022b30cec21
djumae <89045248+djumae@users.noreply.github.com>
2022-06-07 10:57:32 -0700

    got javafx working ESKETTIT

--- Paint.java
+++ Paint.java
@@ -1,2 +1,26 @@
-public class Paint {
+package Paint;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Paint extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResourceAsStream("paintfxml.fxml"));
+
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("tragedy is about to ensue");
+    }
+        
+    
 }
