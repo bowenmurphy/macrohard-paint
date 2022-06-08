3cdaea7869bee44f91a148278fda61448674a82d
djumae <89045248+djumae@users.noreply.github.com>
2022-06-08 11:06:15 -0700

    removed unnecessary classes (cannot figure out gluon scene builder) [11:47]

--- Paint.java
+++ Paint.java
@@ -1,4 +1,4 @@
-package Paint;
+package paint;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
---
+++ paintfxmlController.java
@@ -0,0 +1,26 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class paintfxmlController implements Initializable {
+
+    @FXML
+    private ColorPicker colorpicker;
+
+    @FXML
+    private TextField bsize;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event){
+
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+
+    }
+}
