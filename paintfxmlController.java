dbfe97779dda00cd15ad44fdf74bcb7be3f2f052
djumae <89045248+djumae@users.noreply.github.com>
2022-06-09 10:05:07 -0700

    small changes

--- paintfxmlController.java
+++ paintfxmlController.java
@@ -1,3 +1,5 @@
+package paint;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -5,7 +7,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 
 public class paintfxmlController implements Initializable {
 
@@ -15,10 +16,6 @@ public class paintfxmlController implements Initializable {
     @FXML
     private TextField bsize;
 
-    @FXML
-    private void handleButtonAction(ActionEvent event){
-
-    }
     @Override
     public void initialize(URL url, ResourceBundle rb){
 
